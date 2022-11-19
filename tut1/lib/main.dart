46c21dd2e7a87ad88c7532fa49ee724e814e3f45
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-19 22:49:32 +0545

    Questions added

--- tut1/lib/main.dart
+++ tut1/lib/main.dart
@@ -3,13 +3,16 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questions = ["What is your name?"];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-        title: Text("My first app"),
-      )),
+        appBar: AppBar(
+          title: const Text("My first app"),
+        ),
+        body: const Text("Hello"),
+      ),
     );
   }
 }
