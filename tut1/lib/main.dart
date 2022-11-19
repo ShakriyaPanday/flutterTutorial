4aa75a7c3cb23b9767a9998f2ff14d97adb75426
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-19 23:33:34 +0545

    questions changed by clicking button

--- tut1/lib/main.dart
+++ tut1/lib/main.dart
@@ -2,34 +2,58 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  var questions = ["What is your name?"];
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      if (questionIndex < 4)
+        questionIndex += 1;
+      else
+        questionIndex = 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What is your favorite food ?",
+      "What is your favorite place to visit ?",
+      "What is your favorite book ?",
+      "What is your favorite person ?",
+      "What is your favorite thing to do ?",
+    ];
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text("My first app"),
+          centerTitle: true,
         ),
         body: Center(
           child: Column(
             children: [
-              Text("Question"),
+              Text(questions.elementAt(questionIndex)),
               ElevatedButton(
-                onPressed: (() {}),
-                child: Text("Answer 1"),
+                onPressed: answerQuestion,
+                child: const Text("Answer 1"),
               ),
               ElevatedButton(
-                onPressed: (() {}),
-                child: Text("Answer 2"),
+                onPressed: answerQuestion,
+                child: const Text("Answer 2"),
               ),
               ElevatedButton(
-                onPressed: (() {}),
-                child: Text("Answer 3"),
+                onPressed: answerQuestion,
+                child: const Text("Answer 3"),
               ),
               ElevatedButton(
-                onPressed: (() {}),
-                child: Text("Answer 4"),
+                onPressed: answerQuestion,
+                child: const Text("Answer 4"),
               ),
             ],
           ),
