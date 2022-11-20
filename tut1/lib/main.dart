970cae6ac63b0789342a58fdde1b2a117af7f5e7
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-20 16:26:13 +0545

    quiz.dart added

--- tut1/lib/main.dart
+++ tut1/lib/main.dart
@@ -10,65 +10,71 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final question = const [
+    {
+      'questionText': "What is your favorite food ?",
+      "answerText": [
+        "Pizza",
+        "Mo:Mo",
+        "Samosa",
+        "Bread",
+      ]
+    },
+    {
+      'questionText': "What is your favorite place to visit ?",
+      "answerText": [
+        "Kathmandu",
+        "Bhaktapur",
+        "Lalitpur",
+        "Nuwakot",
+      ]
+    },
+    {
+      'questionText': "What is your favorite book ?",
+      "answerText": [
+        "Feynman lecture",
+        "University Physics",
+        "Grand Design",
+        "Intellegient Investor"
+      ]
+    },
+    {
+      'questionText': "What is your favorite person ?",
+      "answerText": [
+        "Feynman",
+        "Peterson",
+        "Tyson",
+        "˚Kaku",
+      ]
+    },
+    {
+      'questionText': "What is your favorite thing to do ?",
+      "answerText": [
+        "Coding",
+        "Teaching",
+        "Solving math problem",
+      ]
+    },
+  ];
   int _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
-      if (_questionIndex < 4)
-        _questionIndex += 1;
-      else
-        _questionIndex = 1;
+      // if (_questionIndex < 4)
+      _questionIndex += 1;
+      //   else
+      //     _questionIndex = 0;
+      //
     });
+    if (_questionIndex < question.length) {
+      print("We have more questions");
+    } else {
+      print("No more question");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var question = [
-      {
-        'questionText': "What is your favorite food ?",
-        "answerText": [
-          "Pizza",
-          "Mo:MO",
-          "Samosa",
-          "Bread",
-        ]
-      },
-      {
-        'questionText': "What is your favorite place to visit ?",
-        "answerText": [
-          "Kathmandu",
-          "Bhaktapur",
-          "Lalitpur",
-          "Nuwakot",
-        ]
-      },
-      {
-        'questionText': "What is your favorite book ?",
-        "answerText": [
-          "Feynman lecture",
-          "University Physics",
-          "Grand Design",
-          "Intellegient Investor"
-        ]
-      },
-      {
-        'questionText': "What is your favorite person ?",
-        "answerText": [
-          "Feynman",
-          "Peterson",
-          "Tyson",
-          "˚Kaku",
-        ]
-      },
-      {
-        'questionText': "What is your favorite thing to do ?",
-        "answerText": [
-          "Coding",
-          "Teaching",
-          "Solving math problem",
-        ]
-      },
-    ];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -78,17 +84,19 @@ class _MyAppState extends State<MyApp> {
           backgroundColor: Colors.deepOrange[300],
           elevation: 0,
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Question(question[_questionIndex]['questionText'] as String),
-              ...(question[_questionIndex]["answerText"] as List<String>)
-                  .map((answer) {
-                return Answer(_answerQuestion, answer);
-              }).toList()
-            ],
-          ),
-        ),
+        body: _questionIndex < question.length
+            ? Column(
+                children: [
+                  Question(question[_questionIndex]['questionText'] as String),
+                  ...(question[_questionIndex]["answerText"] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  })
+                ],
+              )
+            : Center(
+                child: Text("You did it"),
+              ),
       ),
     );
   }
